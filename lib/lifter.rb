57265aa3b600c446017719892712f99995ce9558
janetmndz <janetmendez.03@gmail.com>
2019-07-21 19:08:26 -0400

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,37 @@
 class Lifter
+  @@all = []
+
   attr_reader :name, :lift_total
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select{|membership| membership.lifter == self}
+  end
+
+  def gyms
+    self.memberships.map{|membership| membership.gym}.uniq
+  end
+
+  def self.avg_total_lifts
+    self.all.reduce(0){|total, lifter| total += lifter.lift_total}.to_f / self.all.count
+  end
+
+  def cost_of_membership
+    self.memberships.reduce(0){|total, membership| total += membership.cost}
+  end
+
+  def sign_up(gym, cost)
+    Membership.new(cost, self, gym)
+  end
+
 end
