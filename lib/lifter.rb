1d0206810088b438daf9647f82bbfca04b3176c6
Ashna Shah-Grover <ashnashahgrover777@gmail.com>
2019-07-22 11:22:49 -0400

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,37 @@
 class Lifter
+
   attr_reader :name, :lift_total
+
+  @@all  = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership.lifter == self}
+  end
+
+  def gyms
+    self.memberships.map {|membership| membership.gym}
+  end
+
+  def total_cost
+    self.memberships.map {|membership| membership.cost}.sum
+  end
+
+  def self.average_lift_total
+    self.all.map {|lifter| lifter.lift_total}.sum.to_f / self.all.size
+  end
+
+  def sign_up(gym, cost)
+    Membership.new(self, gym, cost)
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,22 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+lifter1 = Lifter.new("1", 100)
+lifter2 = Lifter.new("2", 200)
+lifter3 = Lifter.new("3", 300)
+lifter4 = Lifter.new("4", 400)
+
+gym1 = Gym.new("1")
+gym2 = Gym.new("2")
+gym3 = Gym.new("3")
+gym4 = Gym.new("4")
+
+membership1 = Membership.new(lifter1, gym1, 1000)
+membership2 = Membership.new(lifter1, gym2, 2000)
+lifter1.sign_up(gym3, 400)
+lifter2.sign_up(gym3, 800)
+
+
 binding.pry
 
 puts "Gains!"
