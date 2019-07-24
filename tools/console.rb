cb7aabf898b56146184efb9db3c8a95f6abf5a34
Lilaro <larochelle.lisa@gmail.com>
2019-07-23 21:59:11 -0400

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -2,7 +2,17 @@
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
-# test code goes here
+ada = Lifter.new("Ada", 250)
+michael = Lifter.new("Michael", 225)
+
+golds = Gym.new("Golds")
+nysc = Gym.new("NYSC")
+pf = Gym.new("PF")
+
+m1 = Membership.new(ada, golds, 60)
+m2 = Membership.new(ada, nysc, 80)
+m3 = Membership.new(michael, pf, 15)
+m4 = Membership.new(michael, golds, 50)
 
 binding.pry
 
